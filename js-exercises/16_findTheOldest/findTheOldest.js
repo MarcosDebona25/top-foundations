4520fbb2ee4b04cee544ec97eee9f9a91a493d20
Marcos Debona <marcosjd2005@gmail.com>
2026-02-18 12:18:24 -0300

    Ejercicios finales JS

--- js-exercises/16_findTheOldest/findTheOldest.js
+++ js-exercises/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function (arrayObjs) {
+    arrayObjs.forEach(obj => {
+        const fechaMuerte = obj.yearOfDeath || new Date().getFullYear();
+        obj.edad = fechaMuerte - obj.yearOfBirth;
+    });
 
+    return arrayObjs.sort((a, b) => (-(a.edad - b.edad)))[0];
 };
 
 // Do not edit below this line
