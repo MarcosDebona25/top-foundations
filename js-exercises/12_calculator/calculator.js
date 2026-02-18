cfe7191a0ebda0a56a11956d7af59b73fb229780
Marcos Debona <marcosjd2005@gmail.com>
2026-02-18 13:21:43 -0300

    Finales JS v2

--- js-exercises/12_calculator/calculator.js
+++ js-exercises/12_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (arrayNumbers) {
+  return arrayNumbers.reduce((total, num) => total + num, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (arrayNumbers) {
+  return arrayNumbers.reduce((total, num) => total * num, 1);
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return base**exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (a) {
+  if (a===1 || a==0) return 1;
+  return a * factorial(a-1)
 };
 
 // Do not edit below this line
--- js-exercises/13_palindromes/palindromes.js
+++ js-exercises/13_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+/* const palindromes = function (stringArg) {
+    let text = stringArg.replaceAll(" ", "");
 
-};
+    text = text.replace(/[^\w\s]|_/g, '');
+    text = text.toLowerCase(text);
+
+    let j = text.length - 1;
+    let palindrome = true;
+
+    for (let i = 0; i < text.length && palindrome; i++) {
+        if (text[i] !== text[j]) {
+            palindrome = false;
+        }
+        else j--;
+    }
+
+    return palindrome;
+
+}; */
+
+
+const palindromes = function (str) {
+    let text = str.replace(/[^a-zA-Z0-9]+/g, "").toLowerCase();
+    return text === text.split("").reverse().join("");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
